54dd98a8e803653ee6ef078d6073accb26610b66
lawsipan <lawsipan@gmail.com>
2014-07-25 15:54:47 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,18 @@ describe User do
 	before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
 	subject { @user }
-
+	 
+	describe "remember token" do
+    	before { @user.save }
+    	its(:remember_token) { should_not be_blank }
+  	end
+	
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 
 	it {should be_valid}
